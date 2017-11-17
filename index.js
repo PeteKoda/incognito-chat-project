e7d001778f48a99b6265d824d8f39934160d2757
Koda <kodadevep@gmail.com>
2017-11-18 01:57:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(process.env.PORT || 4000,function(){
+  console.log('listening req on port 4000');
+});
+
+// Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection',function(socket){
+  console.log("made socket conn",socket.id)
+
+  socket.on('chat',function(data){
+    io.sockets.emit('chat',data);
+  })
+});
